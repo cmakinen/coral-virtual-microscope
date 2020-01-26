6225910be7996fac759193db97b5099886a09f09
cmakinen <cmakinen@gmu.edu>
2020-01-26 17:25:57 -0500

    Temp draft changes for ajax live search

--- deepzoom_multiserver.py
+++ deepzoom_multiserver.py
@@ -19,7 +19,7 @@
 #
 
 from collections import OrderedDict
-from flask import Flask, abort, make_response, render_template, url_for
+from flask import Flask, request, abort, make_response, render_template, url_for
 from io import BytesIO
 import openslide
 from openslide import OpenSlide, OpenSlideError
@@ -28,6 +28,8 @@ import os
 from optparse import OptionParser
 from threading import Lock
 import csv
+import json
+import sqlite3
 
 SLIDE_DIR = '.'
 SLIDE_CACHE_SIZE = 10
@@ -41,6 +43,18 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('DEEPZOOM_MULTISERVER_SETTINGS', silent=True)
 
+# conn = sqlite3.connect('slides.db')
+# c = conn.cursor()
+# c.execute('''CREATE TABLE slides
+#              (filename text, number text, genus text, species text, stain text, source text, contributor text,
+#              processing text, comments text, date_sent_to_aperio text, infect text, collection_site text,
+#              histopathologic_description text, attachment text)''')
+# conn.commit()
+# conn.close()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+# conn.close()
 
 class PILBytesIO(BytesIO):
     def fileno(self):
@@ -84,33 +98,46 @@ class _Directory(object):
     def __init__(self, basedir, relpath=''):
         self.name = os.path.basename(relpath)
         self.children = []
-        with open('static/gmu-intermediate-slide.csv', 'r') as csvfile:
+        # conn = sqlite3.connect('slides.db')
+        # c = conn.cursor()
+        #
+        with open('static/bette.csv', 'r') as csvfile:
             reader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
             for row in reader:
                 self.children.append(_SlideFile(row))
+        #         c.execute("""INSERT INTO slides (filename, number, genus, species, stain,
+        #         source, contributor, processing, comments, date_sent_to_aperio,
+        #         infect, collection_site, histopathologic_description, attachment)
+        #         VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
+        #                                           (row[5], row[4], row[2], row[3], row[6], row[0],
+        #                                           row[1], row[8], row[9], row[10], row[14], row[16],
+        #                                           row[17], "N/A"))
+        # conn.commit()
+        # conn.close()
 
 class _SlideFile(object):
     def __init__(self, row):
-        self.name = row[9]
-        self.slide_number = row[8]
-        self.slide_description = row[6]
-        self.genus = row[4]
-        self.species= row[5]
-        self.stain = row[10]
-        self.source = row[2]
-        self.contributor = row[3]
-        self.accession_number = row[12]
-        self.processing = row[13]
-        self.comments = row[14]
-        self.date_sent_to_aperio = row[16]
-        self.sample = row[15]
-        self.infect = row[16]
-        self.study = row[17]
-        self.collection_site = row[18]
-        self.histopath_desc = row[19]
+        self.name = row[5]
+        self.slide_number = row[4]
+        # self.slide_description = row[4]
+        self.genus = row[2]
+        self.species= row[3]
+        self.stain = row[6]
+        self.source = row[0]
+        self.contributor = row[1]
+        self.accession_number = row[7]
+        self.processing = row[8]
+        self.comments = row[9]
+        self.date_sent_to_aperio = row[10]
+        self.sample = row[13]
+        self.infect = row[14]
+        self.study = row[15]
+        self.collection_site = row[16]
+        self.histopath_description = row[17]
         self.attachment = "N/A"
 
-        self.url_path = row[9]
+        self.url_path = row[5]
+        print(self.url_path)
 
 @app.before_first_request
 def _setup():
@@ -126,6 +153,7 @@ def _setup():
 
 def _get_slide(path):
     path = os.path.abspath(os.path.join(app.basedir, path))
+    print("The request path is", path)
     if not path.startswith(app.basedir + os.path.sep):
         # Directory traversal
         abort(404)
@@ -138,7 +166,6 @@ def _get_slide(path):
     except OpenSlideError:
         abort(404)
 
-
 @app.route('/')
 def index():
     return render_template('files.html', root_dir=_Directory(app.basedir))
@@ -146,7 +173,9 @@ def index():
 
 @app.route('/<path:path>')
 def slide(path):
+    print("request url",path)
     slide = _get_slide(path)
+    print("request slide",slide)
     slide_url = url_for('dzi', path=path)
     return render_template('slide-fullpage.html', slide_url=slide_url,
             slide_filename=slide.filename, slide_mpp=slide.mpp)
@@ -179,6 +208,31 @@ def tile(path, level, col, row, format):
     resp.mimetype = 'image/%s' % format
     return resp
 
+@app.route("/search")
+def search():
+    print("search request")
+    text = request.args['searchText'] # get the text to search for
+
+    conn = sqlite3.connect('slides.db')
+    c = conn.cursor()
+    t = ('%'+text+'%',)
+    c.execute("select * from slides where filename LIKE ?", t)
+    records = c.fetchall()
+    slides = []
+    for row in records:
+        i = 0
+        slide = {}
+        for key in c.description:
+            slide[key[0]] = row[i]
+            i = i + 1
+        slides.append(slide)
+
+    print(json.dumps({'slides': slides}))
+    conn.commit()
+    conn.close()
+
+    # return as JSON
+    return json.dumps({'slides': slides})
 
 if __name__ == '__main__':
     parser = OptionParser(usage='Usage: %prog [options] [slide-directory]')
